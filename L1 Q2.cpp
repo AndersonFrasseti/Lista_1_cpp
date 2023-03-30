9f8f2a8ae1230221341f32a255e6e638b20219f4
Anderson Frasseti <107011846+AndersonFrasseti@users.noreply.github.com>
2023-03-30 13:39:03 -0300

    Atualizar o L1 Q2.cpp

--- L1 Q2.cpp
+++ L1 Q2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 int main (){
 
-int anosFumando, cigDia, precoCarteira, multi, dividir, soma;
+int anosFumando, cigDia, precoCarteira,cigCarteira, multi, dividir, soma;
 
 cout << "Quantos anos voce fuma?";
 cin >> anosFumando;
@@ -10,9 +10,11 @@ cout << "Quantos cigarros por dia voce fuma?";
 cin >> cigDia;
 cout << "Qual o preco da carteira?";
 cin >> precoCarteira;
+cout << "quantos cigarros vem na carteira?\n";
+cin >> cigCarteira;
 
 multi = anosFumando * precoCarteira;
-dividir = precoCarteira / cigDia;
+dividir = precoCarteira / cigCarteira;
 soma = multi + dividir;
 
 cout << "voce ja gastou\n" << soma << "\nem cigarros";
